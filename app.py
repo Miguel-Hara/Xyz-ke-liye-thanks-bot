838c6500c48c36d362402933c1325b2a4a8ee6c2
ᴍɪɢᴜᴇʟ ᴏ’ʜᴀʀᴀ! <anmol0700k@gmail.com>
2024-07-20 10:10:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask
-from bot import Bot
-import threading
-
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Bot is running!"
+def hello_world():
+    return 'Hello from Tech VJ'
 
-def run_bot():
-    bot = Bot()
-    bot.run()
 
 if __name__ == "__main__":
-    bot_thread = threading.Thread(target=run_bot)
-    bot_thread.start()
-    app.run(host='0.0.0.0', port=8080)
+    app.run()(host='0.0.0.0', port=8080)
